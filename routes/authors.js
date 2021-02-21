6d00f473c07061b38105721f2269ccc3e3e0c579
Haroun Ach <harounacho21@gmail.com>
2021-02-21 16:37:43 +0100

    Search on author

--- routes/authors.js
+++ routes/authors.js
@@ -5,9 +5,15 @@ const Author = require("../models/author");
 
 // All authors route
 router.route("/").get(async (req, res) => {
+  const searchOptions = {};
+
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+
   try {
-    const authors = await Author.find({});
-    res.render("authors/index", { authors: authors });
+    const authors = await Author.find(searchOptions);
+    res.render("authors/index", { authors: authors, searchOptions: req.query });
   } catch (error) {
     res.redirect("/");
   }
