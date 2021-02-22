a87107a9ed70e4489599e31623fe9600c4c2bdd3
Haroun Ach <harounacho21@gmail.com>
2021-02-22 19:22:39 +0100

    DIsplay recently added books to home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
 const router = Router();
+const Book = require("../models/book");
 
 // index page route
-router.route("/").get((req, res) => {
-  res.render("index");
+router.route("/").get(async (req, res) => {
+  let books;
+  try {
+    books = await Book.find().sort({ createdAt: "desc" }).limit(10).exec();
+  } catch {
+    books = [];
+  }
+  res.render("index", { books: books });
 });
 
 module.exports = router;
