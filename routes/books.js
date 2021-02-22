1e0ef12a351376ca1c750a81b177f34eb8a2d5f1
Haroun Ach <harounacho21@gmail.com>
2021-02-22 19:12:15 +0100

    Add book filter to all books page

--- routes/books.js
+++ routes/books.js
@@ -21,9 +21,21 @@ const upload = multer({
 
 // All books route
 router.route("/").get(async (req, res) => {
+  // Build the search query
+  let query = Book.find();
+  if (req.query.title != null && req.query.title != "") {
+    query = query.regex("title", new RegExp(req.query.title, "i"));
+  }
+  if (req.query.publishedBefore != null && req.query.publishedBefore != "") {
+    query = query.lte("publishDate", req.query.publishedBefore);
+  }
+  if (req.query.publishedAfter != null && req.query.publishedAfter != "") {
+    query = query.gte("publishDate", req.query.publishedAfter);
+  }
+
   try {
     // Get books from database
-    const books = await Book.find({});
+    const books = await query.exec();
     res.render("books/index", {
       books: books,
       searchOptions: req.query,
